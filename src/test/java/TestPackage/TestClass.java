5b7e17efa2eed33204d77a922706bb926d84af22
Naveen Kumar <kumar.naveen@magicsw.com>
2021-07-28 23:08:42 +0530

    Initial Commit

---
+++ src/test/java/TestPackage/TestClass.java
@@ -0,0 +1,19 @@
+package TestPackage;
+import org.testng.annotations.Test;
+
+import TopCasting.Runner;
+
+public class TestClass extends Runner{
+
+	
+@Test
+public void TestClass() {
+	
+	Runner.Execution();
+	
+}
+	
+	
+	
+	
+}
